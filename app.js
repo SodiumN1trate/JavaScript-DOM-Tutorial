6b43bb47dd770c671a38ac874561f7707c7f26c1
SodiumN1trate <renarsgausins21@gmail.com>
2021-03-09 15:58:36 +0200

    Tutorial #15

--- app.js
+++ app.js
@@ -36,3 +36,14 @@ addForm.addEventListener('submit', function(e) {
 
     list.appendChild(li)
 })
+
+// hide books
+const hideBox = document.querySelector('#hide')
+hideBox.addEventListener('change', function(e) {
+    if(hideBox.checked){
+        list.style.display = "none"
+    }
+    else{
+        list.style.display = "initial"
+    }
+})
