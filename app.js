78fc279a8192a718bb0ee512d66dfc8fc5b6640f
SodiumN1trate <renarsgausins21@gmail.com>
2021-03-09 14:08:49 +0200

    Tutorial #3

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// Worked in web console!
+var titles = document.getElementsByClassName('title');
+
+//console.log(Array.isArray(titles))
+//console.log(Array.isArray(Array.from(titles)))
+
+Array.from(titles).forEach(function (item) {
+        console.log(item);
+})
+/*
+
+
+
+for(i=0; i < titles.length; i++){
+    console.log(titles[i])
+}
+
+*/
