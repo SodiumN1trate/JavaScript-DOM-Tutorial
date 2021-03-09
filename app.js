d54b3fec22358223771fdc07927b08f4e56d577e
SodiumN1trate <renarsgausins21@gmail.com>
2021-03-09 15:09:37 +0200

    Tutorial #9

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const bookList = document.querySelector('#book-list')
+var btns = document.querySelectorAll('#book-list .delete')
 
-console.log('book-list sibling is:', bookList.nextSibling)
-console.log('book-list next element sibling is:', bookList.nextElementSibling)
+Array.from(btns).forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const li = e.target.parentElement
+        li.parentNode.removeChild(li)
+    })
+})
 
+const link = document.querySelector('#page-banner a')
 
-console.log('book-list previous sibling is:', bookList.previousSibling)
-console.log('book-list previous element sibling is:', bookList.previousElementSibling)
-
-
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br/>Too cool for...'
+link.addEventListener('click', function(e) {
+    e.preventDefault()
+    console.log("Navigation to", e.target.textContent, 'was prevented')
+})
